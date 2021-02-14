669e139674d445ea00b955276b939da3136d8677
takumakume <takuma.kume@pepabo.com>
2021-02-14 22:19:41 +0900

    fix

--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module takumakume/kubectl-vault_client_certificate
 
 go 1.15
 
-require github.com/hashicorp/vault/api v1.0.4 // indirect
+require (
+	github.com/hashicorp/vault/api v1.0.4 // indirect
+	github.com/imdario/mergo v0.3.11 // indirect
+	k8s.io/apimachinery v0.20.2 // indirect
+	k8s.io/client-go v11.0.0+incompatible
+	k8s.io/klog v1.0.0 // indirect
+	k8s.io/utils v0.0.0-20210111153108-fddb29f9d009 // indirect
+)
--- kubeconfig/kubeconfig.go
+++ kubeconfig/kubeconfig.go
@@ -1,12 +1,38 @@
 package kubeconfig
 
+import (
+	"fmt"
+	"log"
+
+	"k8s.io/client-go/tools/clientcmd"
+)
+
 type Kubeconfig struct {
+	clientConfig clientcmd.ClientConfig
 }
 
 func NewKubeconfig() (*Kubeconfig, error) {
-	return &Kubeconfig{}, nil
+	clientConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+		clientcmd.NewDefaultClientConfigLoadingRules(),
+		&clientcmd.ConfigOverrides{},
+	)
+	return &Kubeconfig{clientConfig: clientConfig}, nil
 }
 
-func (k *Kubeconfig) ReadUser(username string) error {
+func (k *Kubeconfig) ReadByUser(username string) error {
+	rawConfig, err := k.clientConfig.RawConfig()
+	if err != nil {
+		return err
+	}
+	users := rawConfig.AuthInfos
+
+	u, ok := users[username]
+	if ok != true {
+		log.Fatalf("User with username %s not found in kube config", username)
+	}
+
+	// なんかの構造体に格納する
+	fmt.Printf("%+v", u)
+
 	return nil
 }
